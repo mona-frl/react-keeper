7d3fe2ca21277002d4f6b7a5a0005e38ccdec60b
Monalisa <monihank@gmail.com>
2023-02-04 23:36:22 +0100

    commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Note's Title</h1>
+            <p>Note's content</p>
+        </div>
+    )
+}
+
+
+export default Note;
