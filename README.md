f4c984f23a2f1b1a33749835c4d67a00f47d887e
Monalisa <monihank@gmail.com>
2023-02-06 10:33:51 +0100

    keeper updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # [The Complete 2023 Web Development Bootcamp - Bootstrap Project](https://www.udemy.com/course/the-complete-web-development-bootcamp/)
 
-A copycat of Keep from Google at starting stage.
+A copycat of Keep from Google at starting stage.03
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Header from './Header.jsx';
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes"
 
 
 function App() {
     return (
         <div>
-        <Header />
-        <Note />
-        <Footer />
+            <Header />
+            {notes.map(note => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content} />
+            ))}
+            <Footer />
         </div>
     )
 };
